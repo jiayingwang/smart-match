108fe388a3f642504e9826cfbd427ff5621da680
jiaying <jiaying@sjzu.edu.cn>
2020-10-08 22:38:12 +0800

    fix jaro bug

--- smart_match/jaro.py
+++ smart_match/jaro.py
@@ -6,29 +6,30 @@ class Jaro:
         
         if not s or not t:
             return 0.0
-        
-        if len(s) > len(t):
-            s, t = t, s
 
         max_dist = (len(t) // 2) - 1
 
-        s_matched = []
-        t_matched = []
         matches = 0
+        hash_s = [0] * len(s) 
+        hash_t = [0] * len(t) 
 
         for i, c in enumerate(s):
             for j in range(max(0, i-max_dist), min(len(t), i+max_dist+1)):
-                if c == t[j]:
+                if c == t[j] and hash_t[j] == 0:
                     matches += 1
-                    s_matched.append(c)
-                    t_matched.insert(j, c)
+                    hash_s[i] = 1
+                    hash_t[j] = 1
                     break
         
         transpositions = 0
-        for m in range(0, len(s_matched)):
-            if(s_matched[m] != t_matched[m]):
-                transpositions += 1
-
+        j = 0
+        for i in range(0, len(s)):
+            if hash_s[i]:
+                while hash_t[j] == 0:
+                    j += 1
+                if s[i] != t[j]:
+                    transpositions += 1
+                j += 1
         return  (matches/len(s) + matches / len(t) + (matches-(transpositions//2)) / matches) / 3
     
     def dissimilarity(self, s, t):
--- tests/test_jaro.py
+++ tests/test_jaro.py
@@ -5,11 +5,12 @@ class TestJaro(unittest.TestCase):
     def setUp(self):
         smart_match.use('jaro')
         
-    def test_jaro_similarity1(self):
+    def test_similarity(self):
         self.assertAlmostEqual(smart_match.similarity('ABC','CBA'), 0.5555555555555555)
         self.assertAlmostEqual(smart_match.similarity('CRATE','TRACE'), 0.7333333333333334)
-        self.assertAlmostEqual(smart_match.similarity('CRATE','TRACE'), 0.7333333333333334)
+        self.assertAlmostEqual(smart_match.similarity('DwAyNE','DuANE'), 0.8222222222222223)
         self.assertAlmostEqual(smart_match.similarity('AABABCAAAC', 'ABAACBAAAC'), 0.9333333333333332)
+        self.assertAlmostEqual(smart_match.similarity('test string1', 'test string2'), 0.9444444444444443)
         
 if __name__=='__main__':
     unittest.main()
