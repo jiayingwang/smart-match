af35563e18138119870c6d2d9d677eac2d9c6426
jiaying <jiaying@sjzu.edu.cn>
2020-10-12 22:24:32 +0800

    update test_smith_waterman_gotoh

--- tests/test_smith_waterman_gotoh.py
+++ tests/test_smith_waterman_gotoh.py
@@ -7,14 +7,14 @@ class TestSmithWatermanGotoh(unittest.TestCase):
         smart_match.use('SWG')
 
     def test_similarity(self):
-        self.assertAlmostEqual(smart_match.similarity('GGTTGACTA', 'TGTTACGG'), 0.3125)
+        self.assertAlmostEqual(smart_match.similarity('GGTTGACTA', 'TGTTACGG'), 0.5625)
         smart_match.set_params(gap=-2, match=3, mismatch=-3)
-        self.assertAlmostEqual(smart_match.similarity('GGTTGACTA', 'TGTTACGG'), 0.2916666666666667)
+        self.assertAlmostEqual(smart_match.similarity('GGTTGACTA', 'TGTTACGG'), 0.5416666666666666)
 
     def test_dissimilarity(self):
-        self.assertAlmostEqual(smart_match.dissimilarity('GGTTGACTA', 'TGTTACGG'), 0.6875)
+        self.assertAlmostEqual(smart_match.dissimilarity('GGTTGACTA', 'TGTTACGG'), 0.4375)
         smart_match.set_params(gap=-2, match=3, mismatch=-3)
-        self.assertAlmostEqual(smart_match.dissimilarity('GGTTGACTA', 'TGTTACGG'), 0.7083333333333333)
+        self.assertAlmostEqual(smart_match.dissimilarity('GGTTGACTA', 'TGTTACGG'), 0.45833333333333337)
 
 if __name__ == '__main__':
     unittest.main()
