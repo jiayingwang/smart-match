6fed2713f372e2ad2df177a6dbb88697873dff11
xinxueqing <72080298+xinxueqing@users.noreply.github.com>
2020-10-12 22:26:13 +0800

    test_monge_elkan
    add test_monge_elkan

--- tests/test_monge_elkan.py
+++ tests/test_monge_elkan.py
@@ -10,6 +10,14 @@ class TestMongeElkan(unittest.TestCase):
         self.assertAlmostEqual(smart_match.similarity(['Hello', 'world'], ['Hero', 'world']), 0.8)
         smart_match.set_params(method='cos')
         self.assertAlmostEqual(smart_match.similarity(['Hello', 'world'], ['Hero', 'world']), 0.7834733547569204)
+        smart_match.set_params(method='EX', level='term')
+        self.assertAlmostEqual(smart_match.similarity('test string1', 'test string2'),  0.5)
+        self.assertAlmostEqual(smart_match.similarity('test', 'test string2'),   0.7071067811865476)
+        self.assertAlmostEqual(smart_match.similarity('', 'test string2'),   0.0)
+        self.assertAlmostEqual(smart_match.similarity('aaa bbb ccc ddd', 'aaa bbb ccc eee'),   0.75)
+        self.assertAlmostEqual(smart_match.similarity('a b c d', 'a b c e'),   0.75)
+        self.assertAlmostEqual(smart_match.similarity('Sam J Chapman', 'Samuel John Chapman'),    0.3333333333333333)
+
 
     def test_dissimilarity(self):
         self.assertAlmostEqual(smart_match.dissimilarity(['Hello', 'world'], ['Hero', 'world']), 0.2)
